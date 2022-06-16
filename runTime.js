e38dcca257fdbff4ae2aecd2dec838aea1429f40
Nebulosa Cat <work@nebulosa-cat.com>
2022-06-17 05:42:10 +0800

    test

--- runTime.js
+++ runTime.js
@@ -1,14 +1,14 @@
 //用途: 紀錄網站架站開始時間
 //頁面每次跳轉都會執行window.pjaxLoaded = function(){}內的程式碼
-//window.pjaxLoaded = (){
-    function NewDate(str) {
+window.pjaxLoaded = (){
+    window.onload function NewDate(str) {
         str = str.split('-');
         var date = new Date();
         date.setUTCFullYear(str[0], str[1] - 1, str[2]);
         date.setUTCHours(0, 0, 0, 0);
         return date;
     }
-    function ageCalc() {
+    window.onload function ageCalc() {
         var hellowWorld = "本站已誕生" + daysold + "天" + hrsold + "小時" + minsold + "分" + seconds + "秒";
         var birthDay = NewDate("2021-07-25");
         var today = new Date();
@@ -24,7 +24,7 @@
         var minsold = Math.floor((hrsold - e_hrsold) * -60); var seconds = Math.floor((minsold - e_minsold) * -60).toString();
         document.getElementById("hellowWorld").innerHTML = "本站已誕生" + daysold + "天" + hrsold + "小時" + minsold + "分" + seconds + "秒";
         setTimeout(ageCalc, 1000);
-    } 
+    }
     ageCalc();
-//}
-//window.pjaxLoaded(); //手動執行 window.pjaxLoaded 讓網頁在第一次載入時也會執行這段腳本
+}
+window.pjaxLoaded(); //手動執行 window.pjaxLoaded 讓網頁在第一次載入時也會執行這段腳本
