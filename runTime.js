9a99b1928538a2868064f18ffb3afc6d590c51b5
Nebulosa Cat <work@nebulosa-cat.com>
2022-06-17 05:25:52 +0800

    test

--- runTime.js
+++ runTime.js
@@ -9,7 +9,7 @@
         return date;
     }
     function ageCalc() {
-        var hellowWorld = "";
+        var hellowWorld = "本站已誕生" + daysold + "天" + hrsold + "小時" + minsold + "分" + seconds + "秒";
         var birthDay = NewDate("2021-07-25");
         var today = new Date();
         var timeold = today.getTime() - birthDay.getTime();
@@ -24,6 +24,7 @@
         var minsold = Math.floor((hrsold - e_hrsold) * -60); var seconds = Math.floor((minsold - e_minsold) * -60).toString();
         document.getElementById("hellowWorld").innerHTML = "本站已誕生" + daysold + "天" + hrsold + "小時" + minsold + "分" + seconds + "秒";
         setTimeout(ageCalc, 1000);
-    } ageCalc();
+    } 
+    ageCalc();
 //}
 //window.pjaxLoaded(); //手動執行 window.pjaxLoaded 讓網頁在第一次載入時也會執行這段腳本
