27815813f123138aa583cf8a89517597c92c4cf5
Nebulosa Cat <work@nebulosa-cat.com>
2022-06-17 03:41:58 +0800

    test

--- runTime.js
+++ runTime.js
@@ -2,25 +2,28 @@
 //用途: 紀錄網站架站開始時間
 //頁面每次跳轉都會執行window.pjaxLoaded = function(){}內的程式碼
 window.pjaxLoaded = function () {
-    var blog_running_days = document.getElementById("blog_running_days");
-    var blog_running_hours = document.getElementById("blog_running_hours");
-    var blog_running_mins = document.getElementById("blog_running_mins");
-    var blog_running_secs = document.getElementById("blog_running_secs");
-    function refresh_blog_running_time() {
-        var time = new Date() - new Date(2021, 7, 25, 0, 0, 0);
-        var d = parseInt(time / 24 / 60 / 60 / 1000);
-        var h = parseInt(time % (24 * 60 * 60 * 1000) / 60 / 60 / 1000);
-        var m = parseInt(time % (60 * 60 * 1000) / 60 / 1000);
-        var s = parseInt(time % (60 * 1000) / 1000);
-        blog_running_days.innerHTML = d;
-        blog_running_hours.innerHTML = h;
-        blog_running_mins.innerHTML = m;
-        blog_running_secs.innerHTML = s;
-    };
-    refresh_blog_running_time();
-    if (typeof (bottomTimeIntervalHasSet) == "undefined") {
-        let bottomTimeIntervalHasSet = true;
-        setInterval(function () { refresh_blog_running_time(); }, 500);
-    };
+    function NewDate(str) {
+        str = str.split('-');
+        var date = new Date();
+        date.setUTCFullYear(str[0], str[1] - 1, str[2]);
+        date.setUTCHours(0, 0, 0, 0);
+        return date;
+    }
+    function helloWorld() {
+        var birthDay = NewDate("2021-07-25");
+        var today = new Date();
+        var timeold = today.getTime() - birthDay.getTime();
+        var sectimeold = timeold / 1000
+        var secondsold = Math.floor(sectimeold);
+        var msPerDay = 24 * 60 * 60 * 1000; var e_daysold = timeold / msPerDay;
+        var daysold = Math.floor(e_daysold);
+        var e_hrsold = (daysold - e_daysold) * -24;
+        var hrsold = Math.floor(e_hrsold);
+        var e_minsold = (hrsold - e_hrsold) * -60;
+        var minsold = Math.floor((hrsold - e_hrsold) * -60); var seconds = Math.floor((minsold - e_minsold) * -60).toString();
+        document.getElementById("momk").innerHTML = "本站已安全运行" + daysold + "天" + hrsold + "小时" + minsold + "分" + seconds + "秒";
+        setTimeout(helloWorld, 1000);
+    } 
+    helloWorld();
 };
 window.pjaxLoaded(); //手動執行 window.pjaxLoaded 讓網頁在第一次載入時也會執行這段腳本
